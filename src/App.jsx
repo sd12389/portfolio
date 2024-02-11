ff5d7aa96f8a55a0d7c9bf84cdd101c5a9dcfc78
sasank <2635sasank@gmail.com>
2024-02-11 10:02:05 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Test from "./Test";
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section id="Homepage">
+      <Navbar/>
+    </section>
+    <section id="Services">Services</section>
+    <section id="Portfolio">Portfolio</section>
+    <section id="Contact">Contact</section>
+    <section id="About">About</section>
+  </div>;
 };
 
 export default App;
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,11 @@
+import "./hero.scss"
+
+const hero = () => {
+  return (
+    <div className="hero">
+      
+    </div>
+  )
+}
+
+export default hero
